26d5e589745e9923753ee2f2b22096592ec15f8e
berfinbastug <berfin.bastug@gmail.com>
2026-06-02 17:00:41 +0200

    still trying to run the demo

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace 'your-repo-name' with your actual GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/berfinbastug/repetition_coherence_project.git',
+  base: '/repetition_coherence_project/',
 })
